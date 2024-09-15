f19513e32facfd6a2d5c0f0694fd2ae6e10f49a4
Noco117 <nicolai.prodanov@gmail.com>
2024-09-15 16:43:34 +0200

    added timeouting functionality and other allowed phrases

---
+++ botsettings.py
@@ -0,0 +1,18 @@
+disallowed_status_components = ["acht", "8", "eight", "ocho", "huit"]
+allowed_exceptions = ["7 >> 8 slay", "7 >> 8 slayy", "7 >> 8 slay fax", "7 >> 8 slayy fax", "7 >> 8 slay ur mom",
+                      "7 >> 8 ur mom slay"]  # The presence of 8 can be tolerated when it is strictly utilised to
+# demonstrate the superiority of 7
+
+accepted_sieben_strings = ["sieben", "ur mom", "ur mom sieben", "sieben ur mom",
+                           "sieben slay", "sieben slayy", "ur mom slay", "ur mom slayy",
+                           "sieben slayyy", "ur mom slayyy", "sieben slayyyyyyy", "ur mom slayyyyyyy",
+                           "slayyyyyyy sieben", "slayyyyyyy ur mom", "slayyyyyyy", "slayyy", "slayy", "slay",
+                           "sieben ur mom slay", "slay sieben ur mom", "ur mom sieben slay", "ur mom slay sieben",
+                           "sieben slay ur mom", "slay ur mom sieben"]  # a few options
+
+timeout_reason = "HERESY IS NOT SLAY, NOT VERY MINDFUL, NOT VERY DEMURE"  # NOT VERY CUTESY
+
+overprivileged_member_message = ("Hurensohnhaftige Überberechtigte, unterlassen Sie das Begehen von Blasphemie \n"
+                                 "7 >> 8 \n"
+                                 "slayy")
+# Gender neutral generic plural? (idk how german works tbh)
--- sieben.py
+++ sieben.py
@@ -1,14 +1,40 @@
 import discord
+import config
+import botsettings
+from datetime import timedelta, datetime
 
-token = "xyz"
-forced_channel_ids = [996928095372185700, 1014956223768035369, 1010674929026469939, 1282087971662729248]  # List of all Channels that "sieben" is strictly enforced in
-#                                           # Currently only includes sieben in GENERAL and TALKI WALKI and
-#                                           # ebenfalls sieben also in TALKI WALKI and Taverne banannnnnnna # sieben in General (not the category but rather ungrouped)
+disallowed_status_components = botsettings.disallowed_status_components
+allowed_exceptions = botsettings.allowed_exceptions
+accepted_sieben_strings = botsettings.accepted_sieben_strings
+timeout_reason = botsettings.timeout_reason
+overprivileged_member_message = botsettings.overprivileged_member_message
 
 
 class SiebenClient(discord.Client):
 
+    async def is_valid_status(self, channel: discord.VoiceChannel, status: str) -> bool:
+        if status is None:
+            return True if "sieben" not in channel.name else False
+        if status in allowed_exceptions:
+            return True
+        for item in disallowed_status_components:
+            if item in status:
+                async for entry in channel.guild.audit_logs(limit=1,
+                                                            action=discord.AuditLogAction.voice_channel_status_update):
+                    if channel.guild.me.top_role > entry.user.top_role and not channel.guild.owner_id == entry.user.id:
+                        await entry.user.timeout(until=(datetime.utcnow() + timedelta(seconds=20)),
+                                                 reason=timeout_reason)
+                    else:
+                        await entry.user.send(overprivileged_member_message)
+                return False
+
+        if "sieben" in channel.name:
+            return status in accepted_sieben_strings
+
+        return True
+
     async def on_voice_state_update(self, member, before, after):
+        print("update")
         if after.channel is None:
             return
 
@@ -16,9 +42,10 @@ class SiebenClient(discord.Client):
             await after.channel.set_status("sieben")
 
     async def on_voice_channel_status_update(self, channel, before, after):
-        if channel.id in forced_channel_ids and channel.members and after != "sieben":
+        if (not await self.is_valid_status(channel, after)) and channel.members:
+            print("siebened")
             await channel.set_status("sieben")
 
 
 client = SiebenClient()
-client.run(token)
+client.run(config.TOKEN)
